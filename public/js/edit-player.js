8b67b3dad3b9064264c10aa1e4277645be4daa75
iamromanh <romanh99@gmail.com>
2017-05-06 01:12:24 -0400

    add ability to add and edit player stats

--- public/js/edit-player.js
+++ public/js/edit-player.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  $('#myBtn').on('click', function() {
-    $('.modal').addClass('is-active').fadeIn('slow')
+  $('#edit-player-button').on('click', function() {
+    $('.edit-player-modal').addClass('is-active').fadeIn('slow')
   })
 
   $('.form-trigger').on('click', function(e) {
