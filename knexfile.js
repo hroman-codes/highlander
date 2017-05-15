d29c87ca8f507c1a6e14c3e988392825013e246d
iamromanh <romanh99@gmail.com>
2017-05-15 14:27:33 -0400

    Refactor production object db url

--- knexfile.js
+++ knexfile.js
@@ -38,13 +38,14 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
+    client: 'pg',
     connection: {
       user: 'dgeblrjcmnovie',
       password: '99a1fcce2305b622eab57f7d7dc5a022da3dc7f802d736cc3b4dcff24f75ebee',
       // database: '@ec2-23-23-223-2.compute-1.amazonaws.com:5432/d1dmfln8ki4jln' + '?ssl=true'
       // database: '@ec2-23-23-223-2.compute-1.amazonaws.com:5432/d1dmfln8ki4jln'
-      database: 'ec2-23-23-223-2.compute-1.amazonaws.com:5432/d1dmfln8ki4jln'
+      database: 'ec2-23-23-223-2.compute-1.amazonaws.com:5432/d1dmfln8ki4jln',
+      ssl: true
     },
     pool: {
       min: 2,
@@ -54,7 +55,6 @@ module.exports = {
       directory: 'data/migrations',
       tableName: 'knex_migrations'
     },
-    ssl: true,
     seeds: {
       directory: 'data/seeds'
     }
