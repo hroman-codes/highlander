62d314955225782dee36318c3833b786fb259b36
iamromanh <romanh99@gmail.com>
2017-05-15 14:19:58 -0400

    Refactor production object db url

--- knexfile.js
+++ knexfile.js
@@ -40,7 +40,6 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: {
-      postgresql: 'postgres://',
       user: 'dgeblrjcmnovie',
       password: '99a1fcce2305b622eab57f7d7dc5a022da3dc7f802d736cc3b4dcff24f75ebee',
       // database: '@ec2-23-23-223-2.compute-1.amazonaws.com:5432/d1dmfln8ki4jln' + '?ssl=true'
